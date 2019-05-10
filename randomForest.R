ce9661989e7bcbd715704d1939c5685a251f72b0
Rodrigo Díaz Lupanow <50117686+progamandoconro@users.noreply.github.com>
2019-05-10 20:49:34 +0100

    Update randomForest.R

--- randomForest.R
+++ randomForest.R
@@ -1,5 +1,3 @@
-
-
 setwd("~/PycharmProjects/titanic")
 
 
@@ -25,7 +23,8 @@ exp<- t[,c(3,5:8,10:12)]
 colnames(exp)
 
 x<- data.frame(target,exp)
-x<-x[sapply(x, function(x) !any(is.na(x)))] 
+#x<-x[sapply(x, function(x) !any(is.na(x)))] 
+#vamos a atender el asunto de los NA 
 
 str(x)
 
@@ -34,6 +33,9 @@ str(x)
 x$Sex<-ifelse(x$Sex=="female",0,1)
 CABIN<- lapply(x$Cabin, substring, 1, 1)
 x$Cabin<-unlist(CABIN)
+
+
+
 table(x$Embarked)
 empty <- x$Embarked == ""
 x$Embarked[empty] <- "S"
@@ -47,27 +49,29 @@ x$Cabin<-ifelse(x$Cabin=="A",1,ifelse(x$Cabin=="B",
                                                                5,ifelse(x$Cabin=="F",
                                                                         6,ifelse(x$Cabin=="G",
                                                                                  7,ifelse(x$Cabin=="T",8,9))))))))
-                                                               
 
-                                                                     
+x<-na.aggregate(x)
+
 str(x)
-
 
 x<-lapply(x, normalize)
 
 x<-as.data.frame(x)
+
+TT<- lapply(t$Ticket, substring, 1, 1)
+x$Ticket<-as.factor(unlist(TT))
 
 set.seed(7)
 
 rand <- sample(1:nrow(x), nrow(x))
 
 # train is our training sample.
-train = x[rand[1:600], ]
+train = x[rand[1:nrow(x)], ]
 
 # Create a holdout set for evaluating model performance.
 # Note: cross-validation is even better than a single holdout sample.
 
-cv = x[601:nrow(x), ]
+#cv = x[601:nrow(x), ]
 
 # Review the outcome variable distribution.CLASIFICACIOON
 table(Y_train, useNA = "ifany")
@@ -78,88 +82,24 @@ set.seed(1)
 RF<-randomForest(as.factor(train$target)~.,
                  data = train[,-1],importance=TRUE,proximity=T,ntree=500)
 
-p<-predict(RF,cv[,-1])
 
-Accuracy(p,cv$target)
-
-########################### intentemos otros modelos que tengo a mano 
-
-set.seed(1)
-
-RF<-svm(as.factor(train$target)~.,data = train[,-1],
-        scale = TRUE,kernel="polynomial",type="C-classification",degree=3)
-
-p<-predict(RF,cv[,-1])
-
-Accuracy(p,cv$target)
-
-
-#################################################
-
-model <- keras_model_sequential()
-
-model %>%
-  layer_dense(units = ncol(train[,-1]), activation = 'relu',
-              input_shape = ncol(train[,-1]), ) %>%
-  layer_dropout(rate = 0.99) %>%
-  layer_dense(units = 10, activation = 'relu') %>%
-  layer_dropout(rate = 0.95) %>%
-  layer_dense(units = 1, activation = 'sigmoid')
-
-history <- model %>% compile(
-  loss = 'binary_crossentropy',
-  optimizer = optimizer_adam(lr=0.001),
-  metrics = c('accuracy')
-)
-
-model %>% fit(
-  as.matrix(train[,-1],dimname=NULL), train$target,
-  epochs =5,
-  batch_size = 20,
-  validation_split = 0.0
-)
-
-#####################################################
-
-#intentemos sacar mas informacion de la data
-
-
-View(t$Name)
-View(t$Ticket)
-TT<- lapply(t$Ticket, substring, 1, 1)
-x$Ticket<-as.factor(unlist(TT))
-table(x$Ticket)
+RF
 
 
 
-#################con la data nueva
 
-setwd("~/PycharmProjects/titanic")
-
-
-library(caret)
-library(randomForest)
-library(MLmetrics)
-library(e1071)
-library (keras)
+######################################################3
 
 
-normalize <- function(x) { 
-  return((x - min(x)) / (max(x) - min(x)))
-}
+test<-read.csv("test.csv")
 
-
-# read data.frame with target and paste it to explanatory variables data.frame
-
-t <- read.csv("train.csv")
-target <- t$Survived
-
-colnames(t)
-exp<- t[,c(3,5:8,10:12)]
+colnames(test)
+exp<- test[,c(2,4:7,9:11)]
 colnames(exp)
 
-x<- data.frame(target,exp)
-x<-x[sapply(x, function(x) !any(is.na(x)))] 
+x<- exp
+#x<-x[sapply(x, function(x) !any(is.na(x)))] 
+#vamos a atender el asunto de los NA 
 
 str(x)
 
@@ -168,6 +108,7 @@ str(x)
 x$Sex<-ifelse(x$Sex=="female",0,1)
 CABIN<- lapply(x$Cabin, substring, 1, 1)
 x$Cabin<-unlist(CABIN)
+
 
 
 table(x$Embarked)
@@ -186,42 +127,34 @@ x$Cabin<-ifelse(x$Cabin=="A",1,ifelse(x$Cabin=="B",
 
 
 
-str(x)
 
+x<-na.aggregate(x)
+
+str(x)
 
 x<-lapply(x, normalize)
 
 x<-as.data.frame(x)
 
-TT<- lapply(t$Ticket, substring, 1, 1)
+TT<- lapply(test$Ticket, substring, 1, 1)
 x$Ticket<-as.factor(unlist(TT))
-x<-data.frame(x,x$Ticket)
 
-set.seed(7)
 
-rand <- sample(1:nrow(x), nrow(x))
+str(x)
 
-# train is our training sample.
-train = x[rand[1:600], ]
+View(x)
 
-# Create a holdout set for evaluating model performance.
-# Note: cross-validation is even better than a single holdout sample.
+xtest <- rbind(train[1,-1 ] , x)
+xtest <- xtest[-1,]
 
-cv = x[601:nrow(x), ]
+Survived<-predict(RF,xtest)
+PassengerId<-test$PassengerId
 
-# Review the outcome variable distribution.CLASIFICACIOON
-table(Y_train, useNA = "ifany")
+submi<-data.frame(PassengerId,Survived)
 
-# Set the seed for reproducibility.
-set.seed(1)
+View(submi)
 
-RF<-randomForest(as.factor(train$target)~.,
-                 data = train[,-1],importance=TRUE,proximity=T,ntree=500)
-
-p<-predict(RF,cv[,-1])
-
-Accuracy(p,cv$target)
-
+write.csv(submi,"Submission.csv",row.names = FALSE)
 
 
 
